c74b01d1957af414af17e6583b9d02b48ddc4e3f
wujingtao <601595686@qq.com>
2017-03-14 00:04:41 +0800

    睡觉提交

--- src/Database.ts
+++ src/Database.ts
@@ -4,7 +4,7 @@
 
 import * as sqlite3 from 'sqlite3';
 
-class SPDatabase {
+class Database {
 
     static get OPEN_READONLY(): number {
         return sqlite3.OPEN_READONLY;
@@ -18,19 +18,55 @@ class SPDatabase {
         return sqlite3.OPEN_CREATE;
     }
 
-    static verbose(): void {
+
+    static verbose(): typeof Database {
         sqlite3.verbose();
+        return Database;
     }
 
-    private db: Promise<sqlite3.Database>;
-
-    constructor(filename: string, mode: number = SPDatabase.OPEN_CREATE | SPDatabase.OPEN_READWRITE, cached: boolean = false) {
-        this.db = new Promise((resolve, reject) => {
+    /**
+     * 连接到指定的sqlite数据库
+     * @static
+     * @param {string} filename sqlite数据库的文健路径
+     * @param {number} [mode=Database.OPEN_CREATE | Database.OPEN_READWRITE] 连接模式
+     * @param {boolean} [cached=false] 是否使用之前打开过的连接
+     * @returns 
+     * 
+     * @memberOf Database
+     */
+    static async connectDB(filename: string, mode: number = Database.OPEN_CREATE | Database.OPEN_READWRITE, cached: boolean = false) {
+        return new Promise<Database>(function (resolve, reject) {
             if (cached) {
-                const db = sqlite3.cached.Database(filename, mode, err => err === null ? resolve(db) : reject(err));
-            } else {
-                const db = new sqlite3.Database(filename, mode, err => err === null ? resolve(db) : reject(err));
+                const db = sqlite3.cached.Database(filename, mode, function (err) {
+                    err ? reject(err) : resolve(new Database(db));
+                });
             }
+            const db = new sqlite3.Database(filename, mode, function (err) {
+                err ? reject(err) : resolve(new Database(db));
+            });
+        });
+    }
+
+    // sqlite数据库连接
+    private db: sqlite3.Database;
+
+    private constructor(db: sqlite3.Database) {
+        this.db = db;
+    }
+
+    async close() {
+        return new Promise<void>((resolve, reject) => {
+            this.db.close(err => {
+                err ? reject(err) : resolve();
+            })
+        });
+    }
+
+    async run(sql: string, ...param: any[]) {
+        return new Promise<void>((resolve, reject) => {
+            this.db.run(sql, param, err => {
+                err ? reject(err) : resolve();
+            })
         });
     }
 }
